a8edf46dd2a400e4f9a62793a77fa0f0b8872e64
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-03 17:23:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    katz_deli.each.with_index(1) do |person, i|
+    katz_deli.each.with_index(2) do |person, i|
       current_line << " #{i}. #{person}"
   end
     puts current_line
@@ -25,3 +25,4 @@ def now_serving(katz_deli)
     katz_deli.shift
   end
 end
+
